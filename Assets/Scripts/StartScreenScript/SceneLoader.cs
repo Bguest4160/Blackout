5221fa5a9670ca2fe36091d9f8333ff890faf539
Bailey Guest <bguest4160@student.pps.net>
2024-11-07 11:39:23 -0800

    done for now

--- Assets/Scripts/StartScreenScript/SceneLoader.cs
+++ Assets/Scripts/StartScreenScript/SceneLoader.cs
@@ -6,15 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject canvasObject1;
+    public GameObject canvasObject2;
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Quit!");
     }
+    public void CharacterCustom()
+    {
+        canvasObject1.SetActive(false);
+        canvasObject2.SetActive(true);
+    }
 
     public void MainSceneLoader()
     {
         SceneManager.LoadScene("Scene 1");
+    }
+
+    public void Back()
+    {
+        canvasObject1.SetActive(true);
+        canvasObject2.SetActive(false);
+
     }
 
 
