dbd694cf87843d4a95be1b12f0bdc33960ad13a9
Nick Auerbach <nickauerbach42@gmail.com>
2025-01-06 11:36:52 -0800

    thowing potions works great now i am very good coder

--- Assets/potionEffectScript.cs
+++ Assets/potionEffectScript.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class potionEffectScript : MonoBehaviour
 {
     [SerializeField] LayerMask players;
     [SerializeField] GameObject particleEffect;
     [SerializeField] GameObject potion;
+    public ParticleSystem ParticleSystem;
+    bool collided = false;
+    public MeshRenderer potionMesh;
+    public CapsuleCollider potionCollider;
 
     // Start is called before the first frame update
     void Start()
@@ -16,20 +21,27 @@ public class potionEffectScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        if (ParticleSystem.isPlaying == true && collided == true)
+        {
+            potionMesh.enabled = false;
+            potionCollider.enabled = false;
+        }
+        if (ParticleSystem.isPlaying == false && collided == true)
+        {
+            potion.SetActive(false);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log(GameManager.activateCollider);
         if (GameManager.activateCollider == true)
         {
-            
             particleEffect.SetActive(true);
-            Debug.Log("collided after throw");
             Collider[] colliders = Physics.OverlapSphere(transform.position, 3f, players);
             //particleEffect.SetActive(false);
-            
+
             foreach (Collider c in colliders)
             {
                 if (c.GetComponent<MovementScript>())
@@ -37,9 +49,7 @@ public class potionEffectScript : MonoBehaviour
                     c.GetComponent<MovementScript>().ChangeStats();
                 }
             }
-            yield return new WaitForSeconds(1f);
-            potion.SetActive(false);
+            collided = true;
         }
-        
     }
 }
