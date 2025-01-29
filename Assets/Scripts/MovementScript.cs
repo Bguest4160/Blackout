147abd0d334627b246216a27668ef30d4ad0d8a1
Bailey Guest <bguest4160@student.pps.net>
2025-01-29 11:34:38 -0800

    made more throwable objects

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -50,7 +50,7 @@ public class MovementScript : NetworkBehaviour
 
     void Update()
     {
-        if (!IsOwner) return;
+        //if (!IsOwner) return;
         UpdateMouse();
         UpdateMove();
     }
