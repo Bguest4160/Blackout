b1426d53448952c3d6866f75e695de465af6a644
Beckett <bolson4867@student.pps.net>
2025-02-12 11:36:59 -0800

    got nicks and tweaked

--- Assets/Scripts/PlayerPickupThrow.cs
+++ Assets/Scripts/PlayerPickupThrow.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerPickupThrow : MonoBehaviour
+public class PlayerPickupThrow : NetworkBehaviour
 {
     [SerializeField] private Transform playerCameraTransform;
     [SerializeField] private LayerMask pickUpLayerMask;
