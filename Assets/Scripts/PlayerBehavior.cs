3bd7a0b64fd6a6b8bdebf23af1c7262c39cec9ee
Bailey Guest <bguest4160@student.pps.net>
2025-03-31 11:11:52 -0700

    added start btn functionality

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -41,6 +41,9 @@ public class PlayerBehavior : NetworkBehaviour {
     // Methods
     
     void Start() {
+       
+
+
         PlayerHealth = new UnitBar(1000, 1000, frontHealthSlider, backHealthSlider, backHealthSliderFill);
         
         rightHitboxRenderer = rightFistHitbox.GetComponent<Renderer>();
