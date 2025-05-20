18e679b3662a28ce11774262d2512da352088cc1
Beckett <bolson4867@student.pps.net>
2025-05-20 10:57:12 -0700

    one door two door high door blue door

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -62,7 +62,7 @@ public class SpawnManager : NetworkBehaviour
 
         if (netTransform != null)
         {
-//            netTransform.Teleport(spawnPoint.position, spawnPoint.rotation);
+//s            netTransform.Teleport(spawnPoint.position, spawnPoint.rotation);
         }
         else
         {
